79d20a6533d806891a2b53783fdf4cda2b6cc0c3
Ruslan Filonchuk <itfreshman@ukr.net>
2021-10-28 17:22:31 +0300

    Додано рішення завдання №2
    Додано рішення завдання №2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listIngredients = document.querySelector("#ingredients");
+
+const elements = ingredients.map(element => {
+  const liEl = document.createElement("li");
+  liEl.textContent = element;
+  liEl.classList.add("item");
+
+  return liEl;
+});
+
+console.log(elements);
+
+listIngredients.append(...elements);

e9130c578e31d89418419d86d654acb283f9ed4b
Ruslan Filonchuk <itfreshman@ukr.net>
2021-10-30 15:29:24 +0300

    Додано рішення завдання №9
    Додано рішення завдання №9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector(".change-color");
+const currentBackgroundColor = document.querySelector(".color");
+const OurBody = document.querySelector("body");
+
+function onChangeColorBtnClick() {
+  OurBody.style.background = getRandomHexColor();
+  currentBackgroundColor.textContent = getRandomHexColor();
+}
+changeColorBtn.addEventListener("click",onChangeColorBtnClick);

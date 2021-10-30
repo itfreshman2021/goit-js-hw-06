1a5d93eb82de30c947ad82c1c3d38f7663835ba6
Ruslan Filonchuk <itfreshman@ukr.net>
2021-10-30 12:07:19 +0300

    Додано рішення завдання №6
    Додано рішення завдання №6

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const textInput = document.querySelector("#validation-input");
+textInput.addEventListener("blur", (event) => {
+   
+    if (event.currentTarget.value.length === Number(textInput.dataset.length)) {
+        textInput.classList.remove("invalid");
+        textInput.classList.add("valid");
+    }
+    else {
+        textInput.classList.remove("valid");
+        textInput.classList.add("invalid");
+    }
+    
+    console.log(textInput.classList);
+    
+    
+});

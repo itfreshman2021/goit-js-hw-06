84c4734143a50b00b1260c0319c8764b71cd56e4
Ruslan Filonchuk <itfreshman@ukr.net>
2021-10-30 10:11:16 +0300

    Додано рішення завдання №5 з виправленнями
    Додано рішення завдання №5 з виправленнями

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,7 @@
 const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
-output.textContent = "Anonymous";
 textInput.addEventListener("input", (event) => {
   output.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") { output.textContent = "Anonymous"; }
+    
 });
